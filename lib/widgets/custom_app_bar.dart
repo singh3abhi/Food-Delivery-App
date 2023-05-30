695f5f968cce7546561d972b828e287cb9f0756d
singh3abhi <singh3abhi@gmail.com>
2023-05-30 19:08:56 +0530

    Added Custom App Bar

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -10,6 +10,7 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      automaticallyImplyLeading: false,
       title: InkWell(
         onTap: () {},
         child: Column(

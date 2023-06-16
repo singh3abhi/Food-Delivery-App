367fc1844436b2108bf518cf17b6e27c4e98196e
singh3abhi <singh3abhi@gmail.com>
2023-06-16 15:10:10 +0530

    Updated

--- lib/widgets/custom_search_bar_text_field.dart
+++ lib/widgets/custom_search_bar_text_field.dart
@@ -15,38 +15,52 @@ class _CustomSearchBarState extends State<CustomSearchBar> {
     return Container(
       color: Colors.white,
       child: Padding(
-        padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-        child: TextField(
-          controller: _textcontroller,
-          decoration: InputDecoration(
-            filled: true,
-            fillColor: const Color.fromARGB(255, 255, 255, 255),
-            hintText: 'Search for dishes & restaurants',
-            hintStyle: const TextStyle(color: Color.fromARGB(255, 112, 111, 116)),
-            contentPadding: const EdgeInsets.all(10),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(14),
-              borderSide: const BorderSide(color: Colors.red),
-            ),
-            suffixIcon: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const SizedBox(
-                  height: 22,
-                  child: VerticalDivider(
-                    color: Color.fromARGB(255, 205, 204, 209),
-                    thickness: 1,
-                    width: 1,
+        padding: const EdgeInsets.fromLTRB(20, 10, 20, 0),
+        child: PhysicalModel(
+          borderRadius: BorderRadius.circular(14),
+          color: Colors.blue,
+          elevation: 0.3,
+          shadowColor: Colors.grey,
+          child: TextField(
+            controller: _textcontroller,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: const Color.fromARGB(255, 255, 255, 255),
+              hintText: 'Search for dishes & restaurants',
+              hintStyle: const TextStyle(color: Color.fromARGB(255, 112, 111, 116)),
+              contentPadding: const EdgeInsets.all(10),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(14),
+                borderSide: const BorderSide(color: Color.fromARGB(255, 217, 218, 219), width: 1.2),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(14),
+                borderSide: const BorderSide(color: Color.fromARGB(255, 217, 218, 219), width: 1.2),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(14),
+                borderSide: const BorderSide(color: Color.fromARGB(255, 217, 218, 219), width: 1.2),
+              ),
+              suffixIcon: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const SizedBox(
+                    height: 22,
+                    child: VerticalDivider(
+                      color: Color.fromARGB(255, 205, 204, 209),
+                      thickness: 1,
+                      width: 1,
+                    ),
                   ),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.mic_outlined,
-                    color: Color.fromARGB(255, 244, 98, 15),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.mic_outlined,
+                      color: Color.fromARGB(255, 244, 98, 15),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
